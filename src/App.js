295013bb2ec64cd6c4d7a06ac7eac61cba97a832
arbettle <151591365+arbettle@users.noreply.github.com>
2023-11-26 20:04:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmployeeForm from './components/EmployeeForm';
+import { useState, useEffect } from 'react';
 
 function App() {
+    const [employees, setEmployees] = useState(() => {
+        const savedEmployees = localStorage.getItem('employees');
+        if (savedEmployees) {
+            return JSON.parse(savedEmployees);
+        } else {
+            return [];
+        }
+    });
+
+    const addEmployee = (newEmployee) => {
+        setEmployees([...employees, newEmployee]);
+    };
+
+    const saveData = () => {
+        localStorage.setItem('employees', JSON.stringify(employees));
+    };
+
+    useEffect(() => {
+        saveData();
+    }, [employees]);
+
     return (
         <BrowserRouter>
             <div>
                 <Routes>
-                    <Route path="/" element={<EmployeeForm />} />
+                    <Route path="/" element={<EmployeeForm addEmployee={addEmployee} />} />
                     <Route path="*" element={<div>Page Not Found</div>} />
                 </Routes>
             </div>
