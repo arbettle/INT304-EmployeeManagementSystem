35c89f5b820a74ac030b81236d9d231810a9c49d
arbettle <151591365+arbettle@users.noreply.github.com>
2023-11-26 19:47:20 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom"
 import './App.css';
-import EmployeeForm from './components/EmployeeForm'; // Import the EmployeeForm component
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmployeeForm from './components/EmployeeForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome <code>to My React project</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-          <EmployeeForm /> {/* Use the EmployeeForm component */}
-    </div>
-  );
     return (
-        <Router>
-            <Header />
-            <Home items={items} />
-            <Routes>
-                <Route path="/" element={<Home items={items} />} />
-                <Route path="*" element={<div>Page Not Found</div>} />
-            </Routes>
-        </Router>
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/" element={<EmployeeForm />} />
+                    <Route path="*" element={<div>Page Not Found</div>} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
